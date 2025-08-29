45446cd758029de079c117158f409b1e82fc563a
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-29 16:57:56 +0530

    feat(controller): add Controller for handling GET requests

--- 02_webApp_project2/src/main/java/in/inxod/controller/TeluskoController.java
+++ 02_webApp_project2/src/main/java/in/inxod/controller/TeluskoController.java
@@ -9,17 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import in.inxod.service.IGreetingService;
 
 @Controller
-@RequestMapping("/v2")
+@RequestMapping("/v2")  // All requests here will start with "/v2"
 public class TeluskoController {
 	
+	 // Service layer is injected automatically by Spring
 	@Autowired
 	IGreetingService service;
 
-	@GetMapping("/greetBirthday")
+	// ----------------- Method -----------------
+		// When user sends a GET request to "/v2/birthday-wish"
+	@GetMapping("/birthday-wish")
 	public String generateWish(Model model) {
 		
 		    String res = service.generateBirthdayWish();
 			model.addAttribute("wish",res);
-		return "birthday";
+		return "greetBirthday";
 	}
+	
+	// user hits this url :-  http://localhost:8080/v2/birthday-wish
 }
