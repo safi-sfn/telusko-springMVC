512e8e7d54a71a3079bf4c318b414a3c03738e56
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-29 19:24:55 +0530

    Initial commit

---
+++ 04_webApp_pathParam/src/main/java/in/inxod/PathParameterApplication.java
@@ -0,0 +1,13 @@
+package in.inxod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathParameterApplication.class, args);
+	}
+
+}
---
+++ 04_webApp_pathParam/src/test/java/in/inxod/PathParameterApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathParameterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
