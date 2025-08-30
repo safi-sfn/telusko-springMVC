40053a7b4b1f5f3082af282880e20229bf2a3eab
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-30 11:56:47 +0530

    initial commit

---
+++ 06_FormData_Binding/src/main/java/in/inxod/FormDataApplication.java
@@ -0,0 +1,13 @@
+package in.inxod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDataApplication.class, args);
+	}
+
+}
---
+++ 06_FormData_Binding/src/test/java/in/inxod/FormDataApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
